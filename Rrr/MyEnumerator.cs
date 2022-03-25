c3ec6756b3811f8c55a9f11cb4b165f5ff1a556d
Егор Богданов <egor5803@bk.ru>
2022-03-25 14:41:21 +0300

    new

---
+++ Rrr/MyEnumerator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rrr
+{
+    internal class MyEnumerator
+    {
+        public IEnumerator<int> IE;
+        public int Value;
+        private Predicate<int>? _predicate;
+        public int Current => Value;
+
+        public int flag;
+        public int takeCount = -1;
+        public int takeTmp = 0;
+
+        public void Dispose()
+        { Console.WriteLine("Dispose!"); }
+
+        public MyEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public MyEnumerator(IEnumerable<int> iE)
+        {
+            IE = iE.GetEnumerator();
+        }
+
+        public bool MoveNext()
+        {
+
+            switch (flag)
+            {
+                case 0:
+                    while (IE.MoveNext() && takeTmp != takeCount)
+                    {
+                        Value = IE.Current;
+                        takeTmp++;
+                        return true;
+                    }
+                    return false;
+                case 1:
+                    while (IE.MoveNext() && takeTmp != takeCount)
+                    {
+                        var tmp = IE.Current;
+                        if (_predicate(tmp))
+                        {
+                            Value = tmp;
+                            takeTmp++;
+                            return true;
+                        }
+                    }
+                    return false;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            Value = default(int);
+        }
+
+        public MyEnumerator MyWhere(Predicate<int> predicate)
+        {
+            _predicate = predicate;
+            flag = 1;
+            return this;
+        }
+
+        public MyEnumerator MyTake(int num)
+        {
+            takeCount = num;
+            return this;
+        }
+    }
+}
--- Rrr/Program.cs
+++ Rrr/Program.cs
@@ -3,18 +3,20 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        foreach(var a in GetEnum().MyWhere(p => p % 2 == 0).MyTake(10))
+
+        MyEnumerator myEnum = new MyEnumerator(GetEnumaration());
+
+        foreach(var a in myEnum.MyWhere(p => p % 2 == 0).MyTake(10))
             Console.WriteLine(a);
 
     }
 
-    public static MyIterator GetEnum()
+    public static IEnumerable<int> GetEnumaration()
     {
-        var tmp = new MyIterator();
+        int i = 0;
         while (true)
         {
-            tmp.Add(0); 
-            return tmp;
+            yield return i++;
         }
     }
 }
